69203298a7cb6214a3e6bc125b684b0d8d47d778
Khahory <angeld05@live.com>
2022-07-22 08:53:03 -0400

    navigate para ejecutarlo en funcionrd

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,21 +1,27 @@
 import React from "react";
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import useFetch from "./services/useFetch";
 import Spinner from "./Spinner";
 import PageNotFound from "./PageNotFound";
 
 export default function Detail() {
-  const {id} = useParams();
-  const {data: product, loading, error} = useFetch('products/'+id);
-  if (error) return <PageNotFound/>
-  if (loading) return <Spinner />
-  
-  return (
-      <div id="detail">
-        <h1>{product.name}</h1>
-        <p>{product.description}</p>
-        <p id="price">${product.price}</p>
-        <img src={`/images/${product.image}`} alt={product.category} />
-      </div>
-  );
+    // constantes
+    const {id} = useParams();
+    const {data: product, loading, error} = useFetch('products/'+id);
+    const navigate = useNavigate();
+    
+    if (error) return <PageNotFound/>
+    if (loading) return <Spinner />
+    
+    return (
+        <div id="detail">
+            <h1>{product.name}</h1>
+            <p>{product.description}</p>
+            <p id="price">${product.price}</p>
+            <p>
+                <button onClick={() => navigate('/cart')} className={'btn btn-primary'}>Add to cart</button>
+            </p>
+            <img src={`/images/${product.image}`} alt={product.category} />
+        </div>
+    );
 }
