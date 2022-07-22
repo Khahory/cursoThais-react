09a5b7332c685702b1bc0721571d213484935a4d
Khahory <angeld05@live.com>
2022-07-22 07:57:56 -0400

    rutas added

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,27 @@
-import React, {useState} from "react";
+import React from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
-import Spinner from "./Spinner";
-import useFetch from "./services/useFetch";
+import Products from "./products";
+import {Route, Routes} from "react-router-dom";
+import Detail from "./Detail";
+import Cart from "./Cart";
 
 export default function App() {
-  //states
-  const [size, setSize] = useState('');
-  
-  //consumir mi hook
-  const {data: productos, loading, error} = useFetch("products?category=shoes")
-  
-  const renderProduct = (p) => {
+    
     return (
-        <div key={p.id} className="product">
-          <a href="/">
-            <img src={`/images/${p.image}`} alt={p.name} />
-            <h3>{p.name}</h3>
-            <p>${p.price}</p>
-          </a>
-        </div>
+        <>
+            <div className="content">
+                <Header />
+                <main>
+                    <Routes>
+                        <Route path={'/'} element={<Products/>}/>
+                        <Route path={'/detail'} element={<Detail/>}/>
+                        <Route path={'/cart'} element={<Cart/>}/>
+                    </Routes>
+                </main>
+            </div>
+            <Footer />
+        </>
     );
-  }
-  
-  const productosFiltrados = size
-      ? productos.filter((p) => p.skus.find((s) => s.size === parseInt(size)))
-      : productos;
-  
-  if (error) throw error;
-  if (loading) return <Spinner />
-  
-  return (
-      <>
-        <div className="content">
-          <Header />
-          <main>
-            <section id="filters">
-              <label htmlFor="size">Filter by Size:</label>{" "}
-              <select id="size" value={size} onChange={(event) => setSize(event.target.value)}>
-                <option value="">All sizes</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-              </select>
-              {size && <h2>Encontre {productosFiltrados.length} items</h2>}
-            </section>
-            <section id={'products'}>
-              {productosFiltrados.map(renderProduct)}
-            </section>
-          </main>
-        </div>
-        <Footer />
-      </>
-  );
 }
---
+++ src/products.jsx
@@ -0,0 +1,48 @@
+import React, {useState} from "react";
+import Spinner from "./Spinner";
+import useFetch from "./services/useFetch";
+
+export default function Products() {
+    //states
+    const [size, setSize] = useState('');
+    
+    //consumir mi hook
+    const {data: productos, loading, error} = useFetch("products?category=shoes")
+    
+    const renderProduct = (p) => {
+        return (
+            <div key={p.id} className="product">
+                <a href="/">
+                    <img src={`/images/${p.image}`} alt={p.name} />
+                    <h3>{p.name}</h3>
+                    <p>${p.price}</p>
+                </a>
+            </div>
+        );
+    }
+    
+    const productosFiltrados = size
+        ? productos.filter((p) => p.skus.find((s) => s.size === parseInt(size)))
+        : productos;
+    
+    if (error) throw error;
+    if (loading) return <Spinner />
+    
+    return (
+        <>
+            <section id="filters">
+                <label htmlFor="size">Filter by Size:</label>{" "}
+                <select id="size" value={size} onChange={(event) => setSize(event.target.value)}>
+                    <option value="">All sizes</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                </select>
+                {size && <h2>Encontre {productosFiltrados.length} items</h2>}
+            </section>
+            <section id={'products'}>
+                {productosFiltrados.map(renderProduct)}
+            </section>
+        </>
+    );
+}
