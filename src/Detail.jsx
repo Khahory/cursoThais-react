509e5a98655990c79227989537c02108b7355123
Khahory <angeld05@live.com>
2022-07-22 09:45:15 -0400

    muestra los size del zapato

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {useNavigate, useParams} from "react-router-dom";
 import useFetch from "./services/useFetch";
 import Spinner from "./Spinner";
@@ -9,15 +9,27 @@ export default function Detail() {
     const {id} = useParams();
     const {data: product, loading, error} = useFetch('products/'+id);
     const navigate = useNavigate();
+    const [sku, setSku] = useState('')
     
-    if (error) return <PageNotFound/>
+    if (!product) return <PageNotFound/>
     if (loading) return <Spinner />
+    if (error) throw error;
     
     return (
         <div id="detail">
             <h1>{product.name}</h1>
             <p>{product.description}</p>
             <p id="price">${product.price}</p>
+            
+            <select id="size" value={sku} onChange={(event) => setSku(event.target.value)}>
+                <option value="">All sizes</option>
+                {product.skus.map((s) => (
+                    <option key={s.sku} value={s.sku}>
+                        {s.size}
+                    </option>
+                ))}
+            </select>
+            
             <p>
                 <button onClick={() => navigate('/cart')} className={'btn btn-primary'}>Add to cart</button>
             </p>
