81437392273714e5756cf87ba0164c7d4643f208
Khahory <angeld05@live.com>
2022-07-22 08:47:40 -0400

    muestra detalles de lo tennis

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import {useParams} from "react-router-dom";
+import useFetch from "./services/useFetch";
+import Spinner from "./Spinner";
+import PageNotFound from "./PageNotFound";
 
 export default function Detail() {
-  return <h1>Detail</h1>;
-
-  // TODO: Display these products details
-  // return (
-  //   <div id="detail">
-  //     <h1>{product.name}</h1>
-  //     <p>{product.description}</p>
-  //     <p id="price">${product.price}</p>
-  //     <img src={`/images/${product.image}`} alt={product.category} />
-  //   </div>
-  // );
+  const {id} = useParams();
+  const {data: product, loading, error} = useFetch('products/'+id);
+  if (error) return <PageNotFound/>
+  if (loading) return <Spinner />
+  
+  return (
+      <div id="detail">
+        <h1>{product.name}</h1>
+        <p>{product.description}</p>
+        <p id="price">${product.price}</p>
+        <img src={`/images/${product.image}`} alt={product.category} />
+      </div>
+  );
 }
