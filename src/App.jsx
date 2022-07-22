40b211835be171cce3c03f931dbab4d7a7fca2c2
Khahory <angeld05@live.com>
2022-07-21 21:07:47 -0400

    creando mi propio hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,16 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
-import {getProducts} from "./services/productService";
 import Spinner from "./Spinner";
-
-const productos = [
-  {
-    "id": 1,
-    "category": "shoes",
-    "image": "shoe1.jpg",
-    "name": "Hiker",
-    "price": 94.95,
-    "skus": [
-      { "sku": "17", "size": 7 },
-      { "sku": "18", "size": 8 }
-    ],
-    "description": "This rugged boot will get you up the mountain safely."
-  },
-  {
-    "id": 2,
-    "category": "shoes",
-    "image": "shoe2.jpg",
-    "name": "Climber",
-    "price": 78.99,
-    "skus": [
-      { "sku": "28", "size": 8 },
-      { "sku": "29", "size": 9 }
-    ],
-    "description": "Sure-footed traction in slippery conditions."
-  },
-  {
-    "id": 3,
-    "category": "shoes",
-    "image": "shoe3.jpg",
-    "name": "Explorer",
-    "price": 145.95,
-    "skus": [
-      { "sku": "37", "size": 7 },
-      { "sku": "38", "size": 8 },
-      { "sku": "39", "size": 9 }
-    ],
-    "description": "Look stylish while stomping in the mud."
-  }
-];
+import useFetch from "./services/useFetch";
 
 export default function App() {
   //states
   const [size, setSize] = useState('');
-  const [producto, setProducto] = useState([]);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
   
-  //call api
-  useEffect(() => {
-    const init = async () => {
-      try {
-        const response = await getProducts('shoes')
-        setProducto(response);
-      } catch (e){
-        setError(e);
-      } finally {
-        setLoading(false);
-      }
-    }
-    init();
-  }, []);
+  //consumir mi hook
+  const {data: productos, loading, error} = useFetch("products?category=shoes")
   
   const renderProduct = (p) => {
     return (
