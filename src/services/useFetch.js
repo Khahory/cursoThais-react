d9ad928b504d11881013014d7b8a6a2b68341252
Khahory <angeld05@live.com>
2022-07-23 11:04:13 -0400

    solo llama api cuando esta montado el componente

--- src/services/useFetch.js
+++ src/services/useFetch.js
@@ -1,30 +1,36 @@
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 
 const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
 export default function useFetch(url){
+    const isMonted = useRef(false);
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
     
     //call api
     useEffect(() => {
+        isMonted.current = true;
         const init = async () => {
             try {
                 const response = await fetch(baseUrl + url);
                 if (response.ok){
                     const json = await response.json();
-                    setData(json);
+                    if (isMonted.current) setData(json);
                 } else {
                     throw response;
                 }
             } catch (e){
-                setError(e);
+                if (isMonted.current) setError(e);
             } finally {
-                setLoading(false);
+                if (isMonted.current) setLoading(false);
             }
         }
         init();
+        
+        return () => {
+            isMonted.current = false;
+        }
     }, [url]);
     
     return {data, error, loading};
